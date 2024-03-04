2df25d1dab30319bd9fdc84154361b6753ab71ab
Pat Nakajima <patnakajima@gmail.com>
2024-03-03 18:07:40 -0800

    Add TextThatFits

---
+++ Sources/pat.swift/TextsThatFits.swift
@@ -0,0 +1,27 @@
+//
+//  TextThatFits.swift
+//
+//
+//  Created by Pat Nakajima on 12/4/22.
+//
+
+import SwiftUI
+
+struct TextThatFits: View {
+	var text: String
+	var fonts: [Font]
+
+	init(_ text: String, fonts: [Font] = [.largeTitle, .title, .title2, .title3, .body, .subheadline, .caption, .caption2]) {
+		self.text = text
+		self.fonts = fonts
+	}
+
+	var body: some View {
+		ViewThatFits {
+			ForEach(0 ..< fonts.count, id: \.self) { i in
+				Text(text)
+					.font(fonts[i])
+			}
+		}
+	}
+}
