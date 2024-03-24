ea580fc2a8cf8e69a02c7ad5a2b3e4fbd6a67a18
Pat Nakajima <patnakajima@gmail.com>
2024-03-24 09:03:10 -0700

    Add snapshot

---
+++ Sources/pat.swift/Snapshot.swift
@@ -0,0 +1,25 @@
+//
+//  Snapshot.swift
+//  Snapshot
+//
+//  Created by Pat Nakajima on 9/7/21.
+//
+
+import SwiftUI
+
+extension View {
+	func snapshot() -> UIImage {
+		let controller = UIHostingController(rootView: self)
+		let view = controller.view
+
+		let targetSize = controller.view.intrinsicContentSize
+		view?.bounds = CGRect(origin: .zero, size: targetSize)
+		view?.backgroundColor = .clear
+
+		let renderer = UIGraphicsImageRenderer(size: targetSize)
+
+		return renderer.image { _ in
+			view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
+		}
+	}
+}
