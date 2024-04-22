022445f150c713e1a9f5a90e565ebeee52f5f78a
Pat Nakajima <patnakajima@gmail.com>
2024-04-22 09:56:32 -0700

    remove navigation hack

--- Sources/pat.swift/Views/SafariView.swift
+++ Sources/pat.swift/Views/SafariView.swift
@@ -44,17 +44,6 @@
 		public func updateUIViewController(_: SFSafariViewController, context _: Context) {}
 	}
 
-	extension UINavigationController: UIGestureRecognizerDelegate {
-		override open func viewDidLoad() {
-			super.viewDidLoad()
-			interactivePopGestureRecognizer?.delegate = self
-		}
-
-		public func gestureRecognizerShouldBegin(_: UIGestureRecognizer) -> Bool {
-			return viewControllers.count > 1
-		}
-	}
-
 	public struct SafariView: View {
 		var url: URL
 		var dismiss: (() -> Void)?
