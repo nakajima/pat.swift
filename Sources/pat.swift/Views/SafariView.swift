58b2cb11973fc8c3eb8d5a1df3c755f6f9e2c29d
Pat Nakajima <patnakajima@gmail.com>
2024-04-22 10:19:50 -0700

    Don't always mess with navigation delegate

--- Sources/pat.swift/Views/SafariView.swift
+++ Sources/pat.swift/Views/SafariView.swift
@@ -6,59 +6,90 @@
 //
 
 #if canImport(SafariServices)
-	import SafariServices
-	import SwiftUI
+import SwiftUI
+import SafariServices
 
-	public struct SafariViewController: UIViewControllerRepresentable {
-		var url: URL
-		var dismiss: (() -> Void)?
+public struct SafariViewController: UIViewControllerRepresentable {
+	var url: URL
+	var dismiss: (() -> Void)?
 
-		public final class Coordinator: NSObject, SFSafariViewControllerDelegate {
-			var url: URL
-			var dismiss: (() -> Void)?
-			var controller: SFSafariViewController
-
-			@MainActor init(url: URL, dismiss: (() -> Void)?) {
-				self.url = url
-				self.dismiss = dismiss
-				self.controller = SFSafariViewController(url: url)
-				super.init()
-				controller.delegate = self
-				controller.modalPresentationStyle = .formSheet
-				controller.dismissButtonStyle = .done
-			}
-
-			public func safariViewControllerDidFinish(_: SFSafariViewController) {
-				dismiss?()
-			}
+	// Without these changes, i was having trouble swiping back from a safari view
+	// in a navigation stack
+	public class NavigatableSafariViewController: SFSafariViewController, UIGestureRecognizerDelegate {
+		public init(url: URL, canDismiss: Bool) {
+			self.canDismiss = canDismiss
+			super.init(url: url, configuration: .init())
 		}
 
-		public func makeCoordinator() -> Coordinator {
-			Coordinator(url: url, dismiss: dismiss)
+		var canDismiss: Bool
+		var oldInteractivePopGestureRecognizerDelegate: (any UIGestureRecognizerDelegate)?
+
+		public override func viewDidAppear(_ animated: Bool) {
+			self.oldInteractivePopGestureRecognizerDelegate = navigationController?.interactivePopGestureRecognizer?.delegate
+			navigationController?.interactivePopGestureRecognizer?.delegate = self
 		}
 
-		public func makeUIViewController(context: Context) -> SFSafariViewController {
-			context.coordinator.controller
+		public override func viewWillDisappear(_ animated: Bool) {
+			navigationController?.interactivePopGestureRecognizer?.delegate = oldInteractivePopGestureRecognizerDelegate
 		}
 
-		public func updateUIViewController(_: SFSafariViewController, context _: Context) {}
+		public func gestureRecognizerShouldBegin(_: UIGestureRecognizer) -> Bool {
+			return canDismiss
+		}
 	}
 
-	public struct SafariView: View {
+	@MainActor public final class Coordinator: NSObject, SFSafariViewControllerDelegate, UIGestureRecognizerDelegate {
 		var url: URL
 		var dismiss: (() -> Void)?
+		var controller: NavigatableSafariViewController
 
-		public init(url: URL, dismiss: (() -> Void)? = nil) {
+		@MainActor init(url: URL, dismiss: (() -> Void)?) {
 			self.url = url
 			self.dismiss = dismiss
+			self.controller = NavigatableSafariViewController(url: url, canDismiss: dismiss != nil)
+			super.init()
+			controller.delegate = self
+			controller.modalPresentationStyle = .pageSheet
+			controller.navigationController?.interactivePopGestureRecognizer?.delegate = self
+			controller.dismissButtonStyle = .done
 		}
 
-		public var body: some View {
-			SafariViewController(url: url, dismiss: dismiss)
-				.ignoresSafeArea(.all)
-				.toolbar(.hidden, for: .navigationBar)
+		public func gestureRecognizerShouldBegin(_: UIGestureRecognizer) -> Bool {
+			print("gestureRecognizerShouldBegin")
+			return true
+		}
+
+		public func safariViewControllerDidFinish(_: NavigatableSafariViewController) {
+			dismiss?()
 		}
 	}
+
+	public func makeCoordinator() -> Coordinator {
+		Coordinator(url: url, dismiss: dismiss)
+	}
+
+	public func makeUIViewController(context: Context) -> NavigatableSafariViewController {
+		context.coordinator.controller
+	}
+
+	public func updateUIViewController(_: NavigatableSafariViewController, context _: Context) {}
+}
+
+public struct SafariView: View {
+	var url: URL
+	var dismiss: (() -> Void)?
+
+	public init(url: URL, dismiss: (() -> Void)? = nil) {
+		self.url = url
+		self.dismiss = dismiss
+	}
+
+	public var body: some View {
+		SafariViewController(url: url, dismiss: dismiss)
+			.ignoresSafeArea(.all)
+			.toolbar(.hidden, for: .navigationBar)
+	}
+}
 
 	#if DEBUG
 		#Preview {
