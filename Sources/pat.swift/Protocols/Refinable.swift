fb83caf8b2fa178d21d22f8fcc6f2eaff6e58bee
Pat Nakajima <patnakajima@gmail.com>
2024-04-23 22:15:14 -0700

    add refinable

---
+++ Sources/pat.swift/Protocols/Refinable.swift
@@ -0,0 +1,37 @@
+// https://github.com/groue/GRDB.swift/blob/master/GRDB/Utils/Refinable.swift
+/// A marker protocol for refinable types.
+///
+/// For example:
+///
+///     struct Player {
+///         var name: String
+///         var score: Int
+///     }
+///
+///     extension Player: Refinable { }
+///
+///     let player = Player(name: "Arthur", score: 1000)
+///
+///     // Player(name: "Arthur", score: 100)
+///     let newPlayer = player.with {
+///         $0.score = 100
+///     }
+public protocol Refinable { }
+
+public extension Refinable {
+		/// Returns self modified with the *update* function.
+		///
+		/// For example:
+		///
+		///     let player = Player(name: "Arthur", score: 1000)
+		///     let newPlayer = player.with {
+		///         $0.score = 100
+		///     }
+		///     newPlayer.name  // "Arthur"
+		///     newPlayer.score // 100
+		func with(_ update: (inout Self) throws -> Void) rethrows -> Self {
+				var result = self
+				try update(&result)
+				return result
+		}
+}
