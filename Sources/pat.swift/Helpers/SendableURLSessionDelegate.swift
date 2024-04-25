52b1b8a2806420bf6ec78c6f9febef6f35c9c080
Pat Nakajima <patnakajima@gmail.com>
2024-04-24 22:55:37 -0700

    wrong delegate

--- Sources/pat.swift/Helpers/SendableURLSessionDelegate.swift
+++ Sources/pat.swift/Helpers/SendableURLSessionDelegate.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 // To silence warnings about session delegates not being sendable, can pass this in.
-public final class SendableURLSessionDelegate: NSObject, URLSessionDelegate, Sendable {
+public final class SendableURLSessionDelegate: NSObject, URLSessionTaskDelegate, Sendable {
 	override public init() { }
 }
